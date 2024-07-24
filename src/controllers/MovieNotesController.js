c51ed87e3592d1b6c7990468d292001037955034
iuryescano <iurycordeiro15@gmail.com>
2024-07-24 20:17:56 -0300

    Added Show, delete and index in movieNotesRouter

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -25,6 +25,46 @@ class MovieNotesController {
 
         return response.status(201).json();
     }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const movieNote = await knex("movie_notes").where({ id }).first(); //para trazer uma nota somente em especifico que eu quero
+        const movieTags = await knex("movie_tags").where({ movie_id : id }).orderBy("name"); //trazendo as tags para o meu show no caso so pra poder visualizar mesmo
+
+        return response.json({
+            ...movieNote,
+            movieTags
+        });
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("movie_notes").where({ id }).delete(); //deletando a MovieNote sendo assim a tag precisa ser deletada tbm pq esta em cascade
+
+        return response.status(204).json();
+    }
+
+    async index(request, response) {
+        const { title, user_id, tags } = request.query;
+
+        let movieNotes;
+
+        if(tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+            
+            movieNotes = await knex("movie_tags").whereIn("name", filterTags)
+            
+        }else {
+            movieNotes = await knex("movie_notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+            .orderBy("title");
+        }
+
+        return response.json(movieNotes);
+    }
 }
 
 module.exports = MovieNotesController;
--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -6,6 +6,9 @@ const movieNotesRoutes = Router();
 
 const movieNotesController = new MovieNotesController();
 
+movieNotesRoutes.get("/" , movieNotesController.index); //nao precisa do user_id pq ja esta sendo passado pela query 
 movieNotesRoutes.post("/:user_id" , movieNotesController.create);
+movieNotesRoutes.delete("/:id" , movieNotesController.delete);
+movieNotesRoutes.get("/:id" , movieNotesController.show);
 
 module.exports = movieNotesRoutes;
